dec1cc9cccc680686fbbb1117bacdcd090b60dfa
Bokov Roman <r.bokov@artvision.ai>
2025-09-01 06:33:05 +0300

    update

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -6,11 +6,9 @@ def test_post_v1_account(account_helper, prepare_user):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
-    a= account_helper.create_new_user(login=login, password=password, email=email)
-    print(a)
+    account_helper.create_new_user(login=login, password=password, email=email)
     resource = account_helper.user_login(login=login, password=password,validate_response=True)
 
-    print(resource)
 
 
 
