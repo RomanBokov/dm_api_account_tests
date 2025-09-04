eecf847855193e21075f183f3b79f9847643c2ae
Bokov Roman <r.bokov@artvision.ai>
2025-09-04 16:11:52 +0300

    Add result many conflict and fix bugs

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,5 +1,9 @@
+import uuid
+import pytest
 
+from checkers.http_checkers import check_status_code_http
 from checkers.post_v1_account import PostV1Account
+
 
 
 def gen_random_string(length):
@@ -19,12 +23,13 @@ def test_post_v1_account(
         ):
     # Регистрация пользователя
 
-    login = prepare_user.login
-    password = prepare_user.password
-    email = prepare_user.email
-    account_helper.create_new_user(login=login, password=password, email=email)
-    response = account_helper.user_login(login=login, password=password, validate_response=True)
-    PostV1Account.check_response_values(response)
+    login = login
+    password = password
+    email = email
+    with check_status_code_http(expected_status_code=expected_status, expected_message=expected_message):
+        account_helper.create_new_user(login=login, password=password, email=email)
+        response = account_helper.user_login(login=login, password=password, validate_response=True)
+        PostV1Account.check_response_values(response)
 
 
 
