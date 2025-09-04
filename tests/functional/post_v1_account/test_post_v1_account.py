89d67004824da18a2e40d54df9bce998723f0640
Bokov Roman <r.bokov@artvision.ai>
2025-09-04 16:54:31 +0300

    update fail mearge

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -2,7 +2,6 @@ import allure
 
 from checkers.post_v1_account import PostV1Account
 
-
 ### для словаря используется has_key . a для объекта has_property, starts_with - проверяем начало по регулярке
 ### all_of - выполнение всех проверок
 @allure.suite("Тесты на проверку метода POST v1/account")
