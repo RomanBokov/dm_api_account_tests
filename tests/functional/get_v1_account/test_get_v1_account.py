3f36e43036a40267fc7ab6a3c1dad5ac9eba9d56
Bokov Roman <r.bokov@artvision.ai>
2025-09-04 13:03:11 +0300

    Fix bug lib and login

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -3,7 +3,7 @@ from checkers.get_v1_account import GetV1Account
 
 def test_get_v1_account(auth_account_helper):
     response = auth_account_helper.dm_account_api.account_api.get_v1_account()
-    GetV1Account.check_response_values_get_auth_account_helper(response=response)
+    GetV1Account.check_response_values_get_auth_account_helper(response=response, login='user90')
 
 
 
@@ -14,7 +14,7 @@ def test_get_v1_account_no_auth(account_helper):
 # Получить информацию о пользователе (используя авторизованный клиент)
 def test_open_information_user(auth_account_helper):
     response =auth_account_helper.dm_account_api.account_api.get_v1_account()
-    GetV1Account.check_response_values_get_auth_account_helper(response=response)
+    GetV1Account.check_response_values_get_auth_account_helper(response=response, login='user90')
 
 
 
